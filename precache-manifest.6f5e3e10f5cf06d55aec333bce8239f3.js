2cfa7ba52f8e13044a9ea50cc3f01bbaf3ab553b
Jacob Krol <phone12.jk@gmail.com>
2020-08-03 12:08:34 -0500

    Updates

---
+++ precache-manifest.6f5e3e10f5cf06d55aec333bce8239f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bbf3207d71f035ef60b5cd0e9ce9f69",
+    "url": "/play-nertz-offline/index.html"
+  },
+  {
+    "revision": "416c2cda2eb524137bf5",
+    "url": "/play-nertz-offline/static/css/main.eff9e58c.chunk.css"
+  },
+  {
+    "revision": "2550e8ce808bac862d08",
+    "url": "/play-nertz-offline/static/js/2.f1c30793.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/play-nertz-offline/static/js/2.f1c30793.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "416c2cda2eb524137bf5",
+    "url": "/play-nertz-offline/static/js/main.437f2992.chunk.js"
+  },
+  {
+    "revision": "3c6da80527e13b909fae",
+    "url": "/play-nertz-offline/static/js/runtime-main.2c52f157.js"
+  }
+]);
